58e90ba29fdd2d489f95237f5fa7b2a67f006ac3
Josh Payette <hello@joshpayette.dev>
2025-05-25 22:16:49 -0400

    Removed development code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,6 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import { ThemeProvider } from '@/components/theme/theme-provider';
-import { NuqsAdapter } from 'nuqs/adapters/next/app';
-import { ReactQueryProvider } from '@/lib/react-query/react-query-provider';
-import { SidebarProvider } from '@/app/_navigation/sidebar-provider';
-import { Header } from '@/app/_navigation/header';
-import { Toaster } from 'sonner';
 import PlausibleProvider from 'next-plausible';
 
 const geistSans = Geist({
@@ -36,19 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <PlausibleProvider domain="toolkits.gg">
-          <NuqsAdapter>
-            <ThemeProvider>
-              <ReactQueryProvider>
-                <SidebarProvider>
-                  <Header />
-                  <main className="flex flex-1 flex-col gap-4 p-4 pt-0">
-                    {children}
-                  </main>
-                </SidebarProvider>
-                <Toaster expand />
-              </ReactQueryProvider>
-            </ThemeProvider>
-          </NuqsAdapter>
+          <ThemeProvider>{children}</ThemeProvider>
         </PlausibleProvider>
       </body>
     </html>
