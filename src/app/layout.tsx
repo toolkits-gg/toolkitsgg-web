1a77657e5924e5e78a859177ae3fc8f7b1f564d6
Josh Payette <hello@joshpayette.dev>
2025-05-26 07:22:16 -0400

    Removed umami script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Script src="https://umami.toolkits.gg/script.js" />
         <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
