669f45de6ec4fe809077833896febc190e2a2ece
Josh Payette <hello@joshpayette.dev>
2025-08-01 20:31:56 -0400

    Added tooltip

--- src/app/_navigation/components/favorite-game-button.tsx
+++ src/app/_navigation/components/favorite-game-button.tsx
@@ -7,6 +7,7 @@ import { Form } from '@/components/form/form';
 import { SubmitButton } from '@/components/form/submit-button';
 import { EMPTY_ACTION_STATE } from '@/components/form/utils/to-action-state';
 import { toggleFavoriteGame } from '@/features/game/actions/toggle-favorite-game';
+import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/tooltip';
 
 type FavoriteGameButtonProps = {
   gameId: GameId;
@@ -22,17 +23,31 @@ const FavoriteGameButton = ({
     EMPTY_ACTION_STATE
   );
 
+  if (isFavorite) {
+    return (
+      <Form action={action} actionState={actionState}>
+        <Tooltip>
+          <TooltipTrigger>
+            <SubmitButton color="accent2" isPending={isPending}>
+              <LucideHeart fill="white" className="h-4 w-4" />
+            </SubmitButton>
+          </TooltipTrigger>
+          {!isPending && <TooltipContent>Unfavorite game</TooltipContent>}
+        </Tooltip>
+      </Form>
+    );
+  }
+
   return (
     <Form action={action} actionState={actionState}>
-      {isFavorite ? (
-        <SubmitButton color="accent2" isPending={isPending}>
-          <LucideHeart fill="white" className="h-4 w-4" />
-        </SubmitButton>
-      ) : (
-        <SubmitButton isPending={isPending}>
-          <LucideHeartPlus className="h-4 w-4" />
-        </SubmitButton>
-      )}
+      <Tooltip>
+        <TooltipTrigger>
+          <SubmitButton isPending={isPending}>
+            <LucideHeartPlus className="h-4 w-4" />
+          </SubmitButton>
+        </TooltipTrigger>
+        {!isPending && <TooltipContent>Favorite game</TooltipContent>}
+      </Tooltip>
     </Form>
   );
 };
