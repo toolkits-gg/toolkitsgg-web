bfc22f48ae95b45ae6d00a987bb927db48cfb028
Josh Payette <hello@joshpayette.dev>
2025-08-12 20:52:42 -0400

    Feat: Implement GameSwitcher using Mantine components

--- src/components/navigation/components/GameSwitcher.tsx
+++ src/components/navigation/components/GameSwitcher.tsx
@@ -1,14 +1,24 @@
 'use client';
 
 import type { GameId } from '@prisma/client';
-import { noGameConfig } from '@/features/game/constants';
+import { allGameConfigs, noGameConfig } from '@/features/game/constants';
 import { useAppTheme } from '@/features/theme/hooks/use-app-theme';
 import { usePathname } from 'next/navigation';
 import { toGameConfig } from '@/features/game/utils/game-id';
 import React from 'react';
-import { Group, UnstyledButton } from '@mantine/core';
+import {
+  Anchor,
+  Box,
+  Flex,
+  Group,
+  Menu,
+  Text,
+  UnstyledButton,
+} from '@mantine/core';
 import classes from './GameSwitcher.module.css';
 import { IconChevronRight } from '@tabler/icons-react';
+import { allThemeClassNames } from '@/features/theme/constants';
+import Link from 'next/link';
 
 type UseActiveGameConfigArgs = {
   gameId: GameId | undefined;
@@ -52,14 +62,38 @@ const GameSwitcher = ({ gameId }: GameSwitcherProps) => {
   });
 
   return (
-    <UnstyledButton className={classes.game}>
-      <Group>
-        <div className={classes.logo}>{activeGameConfig.logo(48)}</div>
-
-        <IconChevronRight size={14} stroke={1.5} />
-        {/** TODO */}
-      </Group>
-    </UnstyledButton>
+    <Menu
+      width={300}
+      position="bottom-start"
+      transitionProps={{ transition: 'fade-down', duration: 150 }}
+    >
+      <Menu.Target>
+        <Flex align="center" justify="center" gap="sm">
+          <Box className={classes.logo} p={5}>
+            {activeGameConfig.logo(48)}
+          </Box>
+          <IconChevronRight size={14} stroke={1.5} />
+        </Flex>
+      </Menu.Target>
+      <Menu.Dropdown className={classes.menu}>
+        {allGameConfigs
+          .filter((gameConfig) => gameConfig.id !== noGameConfig.id)
+          .map((gameConfig) => (
+            <Menu.Item key={gameConfig.id} className={classes.menuItem}>
+              <Anchor
+                href={gameConfig.path}
+                component={Link}
+                className={classes.link}
+              >
+                <Flex align="center" justify="center" gap="sm">
+                  {gameConfig.logo(48)}
+                  <Text size="sm">{gameConfig.name}</Text>
+                </Flex>
+              </Anchor>
+            </Menu.Item>
+          ))}
+      </Menu.Dropdown>
+    </Menu>
   );
 };
 
