43d894a0db89e35f9ce2d18ec8ab8a96c5773bb1
Josh Payette <hello@joshpayette.dev>
2026-07-04 20:02:23 -0400

    Fixed home page not resetting gameId.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,16 @@
 import { Title } from "@mantine/core";
 import { createFileRoute } from "@tanstack/react-router";
+import { useEffect } from "react";
+import { setGame } from "#/features/game/store.ts";
+
+const HomePage = () => {
+	useEffect(() => setGame("none"));
+
+	return <Title order={1}>Home Page</Title>;
+};
 
 const Route = createFileRoute("/")({
-	component: () => <Title order={1}>Home Page</Title>,
+	component: HomePage,
 });
 
 export { Route };
