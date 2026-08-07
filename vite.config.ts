ff3bd0928383aa6cc819aa7959e07c6ad9468bfa
Josh Payette <hello@joshpayette.dev>
2026-08-06 21:30:01 -0400

    Updated deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 
 // biome-ignore lint/style/noDefaultExport: <not a problem in the config>
@@ -18,6 +19,7 @@ export default defineConfig({
 				},
 			},
 		}),
+		nitro(),
 		// react's vite plugin must come after start's vite plugin
 		viteReact(),
 	],
