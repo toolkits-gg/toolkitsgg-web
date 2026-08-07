9e96916566c0ec9ed568ec9d4151c18d72fdbd20
Josh Payette <hello@joshpayette.dev>
2026-08-07 15:06:18 -0400

    Added DATABASE_URL_UNPOOLED environment variable.

--- prisma.config.ts
+++ prisma.config.ts
@@ -12,6 +12,6 @@ export default defineConfig({
     seed: 'tsx prisma/seed.ts',
   },
   datasource: {
-    url: env('DATABASE_URL'),
+    url: process.env.DATABASE_URL_UNPOOLED ?? env('DATABASE_URL'),
   },
 });
